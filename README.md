267d786ff36c3bc7263756792591e8f84a97e7bf
Jason <iJason92@yahoo.com>
2016-12-08 14:05:59 +0800

    添加生成App的icon的shell脚本

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 
 # 列表
 
-|功能|文件名|详情|
-|:-:|:-:|:-:|
-|获取.ipa文件的Info.plist | look_info.sh |[shell脚本查看.ipa文件的Info.plist](https://ishepherdminer.github.io/2016/11/30/shell脚本查看-ipa文件的Info-plist/)|
+|功能|文件名|
+|:--|:-:|
+|[查看.ipa文件的Info.plist](https://ishepherdminer.github.io/2016/11/30/shell脚本查看-ipa文件的Info-plist/) | look_info.sh |
+|快速生成App的Icon图标|generate_app_icon.sh
 
 
---
+++ generate_app_icon.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+scale_config=(
+        "iTunesArtwork.png" 512
+        "iTunesArtwork@2x.png" 1024
+        "Icon-60@2x.png" 120
+        "Icon-60@3x.png" 180
+        "Icon-76.png" 76
+        "Icon-76@2x.png" 152
+        "Icon-Small-40.png" 40
+        "Icon-Small-40@2x.png" 80
+        "Icon-Small-40@3x.png" 120
+        "Icon-Small.png" 29
+        "Icon-Small@2x.png" 58
+        "Icon-Small@3x.png" 87
+     )
+
+num=${#scale_config[@]}
+
+for (( i = 0; i < num; i++ )); do
+    cache_img=${scale_config[i]}
+    cp logo.png ${cache_img}
+    i=$((${i}+1))
+    sips -Z ${scale_config[i]}x${scale_config[i]} ${cache_img}
+done
