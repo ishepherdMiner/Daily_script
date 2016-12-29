792be49db7a69df90fa7f92ae13720912dbeadbc
Jason <iJason92@yahoo.com>
2016-12-30 07:32:48 +0800

    Daily_sh_set_1483054347

--- github_commit_repo.sh
+++ github_commit_repo.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 DATE=$(date +%s)
+CheckList=('Daily_knowledge_set','Daily_sh_set','Daily_ui_objc_set','Daily_ui_set')
 TARGET='/Users/Jason/Developer/01-Engineer/Github/Me'
 # echo ${DATE}
 cd ${TARGET}
 for file in ${TARGET}/*
 do
   cd ${file}
-  echo ${file} 
+  echo ${file##*/}
   git remote -v | grep push | awk '{print $2}' | git push
 done
 
 for file in ${TARGET}/*
 do
+if echo "${CheckList[@]}" | grep -w ${file##*/} &>/dev/null; then
   cd ${file}
   # echo ${file}
   git status | grep "nothing to commit" > /dev/null 2>&1
@@ -24,4 +26,5 @@ do
   else
     echo "没有更改:"${file##*/}
   fi
+fi
 done
