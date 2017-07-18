35654ecce5231992ac1c754f376ef0df1924fe8a
ishepherdMiner <iJason92@yahoo.com>
2017-07-18 22:59:22 +0800

    Daily_sh_set_1500389910

--- generate_app_icon.sh
+++ generate_app_icon.sh
@@ -3,10 +3,21 @@
 scale_config=(
         "iTunesArtwork.png" 512
         "iTunesArtwork@2x.png" 1024
+        "Icon-60.png" 60
         "Icon-60@2x.png" 120
         "Icon-60@3x.png" 180
+        "Icon-72.png" 72
+        "Icon-72@2x.png" 144        
         "Icon-76.png" 76
         "Icon-76@2x.png" 152
+        "Icon-76@3x.png" 228
+        "Icon-114.png" 114
+        "Icon-144.png" 144
+        "Icon-Small-50.png" 50
+        "Icon-Small-50@2x.png" 100
+        "Icon-Spotlight-40.png" 40
+        "Icon-Spotlight-40@2x.png" 80
+        "Icon-Spotlight-40@3x.png" 120
         "Icon-Small-40.png" 40
         "Icon-Small-40@2x.png" 80
         "Icon-Small-40@3x.png" 120
@@ -15,6 +26,9 @@ scale_config=(
         "Icon-Small@3x.png" 87
         "Icon-10@2x.png" 40
         "Icon-10@3x.png" 60
+        "Icon.png" 57
+        "Icon@2x.png" 114
+        "Icon-57.png" 57
      )
 
 num=${#scale_config[@]}
